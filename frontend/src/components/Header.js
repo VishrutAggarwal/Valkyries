c5f19a0005e00634fbdbb1487d65537901955a5d
Vishrut Aggarwal <vishrut.agg@gmail.com>
2023-01-07 14:29:10 +0530

    add: add header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div id='header-component'>
+        <div>
+            <img src=''></img>
+            <p className='heading-highest'>Valkyries</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
