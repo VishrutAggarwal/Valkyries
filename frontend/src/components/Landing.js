5acdd24519bad7679d66721c6fe672bcb8ac3725
Vishrut Aggarwal <vishrut.agg@gmail.com>
2023-01-07 14:29:27 +0530

    add: add landing page

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        <div id='about-section'>
+
+        </div>
+        <div id='login-register'>
+            <div id='login' className='landing-card'>
+
+            </div>
+            <div id='register' className='landing-card'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
