89f99a15a549f5b7b2ce3a7db57915f99c86880b
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-01-08 16:09:42 +0530

    update: update header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import '../styles/Header.css'
+import React from 'react';
+// import '../styles/Header.css'
+import '../styles/Dashboard.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div id='header-component'>
-        <div>
-            <img src=''></img>
-            <p className='heading-highest'>Valkyries</p>
-        </div>
+      <div class="wrapper">
+        <nav>
+          <div class="content">
+            <div class="logo">
+              <a href="/#">Valkyries</a>
+            </div>
+            <ul class="links">
+              <li><a href="/">Home</a></li>
+              <li><a href="/register">Register</a></li>
+              <li><a href="/login">Login</a></li>
+              <li><a href="/resources">Resources</a></li>
+              <li>
+                <a href="/helpline" class="desktop-link">Helpline</a>
+              </li>
+              <li>
+                <a href="/forum" class="desktop-link">Forum</a>
+              </li>
+              <li><a href="/#">About us</a></li>
+              <li><a href="/#">Contact us</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
