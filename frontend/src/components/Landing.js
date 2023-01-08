dc09efeefa4d4ef03bada3757c04111127f51f71
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-01-08 16:10:25 +0530

    update: update landing component

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import '../styles/Landing.css'
+import '../styles/Dashboard.css'
+import Header from './Header'
+import Footer from './Footer'
+import { Link } from 'react-router-dom'
+
 
 function Landing() {
   return (
     <div id='landing-page'>
-        <div id='about-section'>
-
-        </div>
-        <div id='login-register'>
-            <div id='login' className='landing-card'>
-
+      {/* <Header/> */}
+      <div id='about-section'>
+        <h1>Valkyries</h1>
+        <img className='img-landing' src='https://media.discordapp.net/attachments/794519160746541108/1061560277168029756/valky.png'></img>
+        <h5>For our fierce warriors and their never diminishing light.</h5>
+      </div>
+      <div id='page'>
+        <div id='container'>
+          <div className='card'>
+            <Link to='/login'>
+            <p>Login</p>
+              <img className='card-imagess' src='https://cdn.discordapp.com/attachments/794519160746541108/1061576560043380746/295128.png'></img>
+            </Link>
             </div>
-            <div id='register' className='landing-card'>
-
-            </div>
+            <div className='card'>
+            <Link to='/register'>
+            <p>Register</p>
+              <img className='card-imagess' src='https://media.discordapp.net/attachments/794519160746541108/1061576977099800616/203-2035339_register-user-register-online-icon-png.png'></img>
+            </Link>
+          </div>
         </div>
-    </div>
-  )
+      </div>
+      </div>
+      )
 }
 
-export default Landing
+      export default Landing
