414b48161dc64319272f93975104797e74c78f01
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-01-08 16:09:22 +0530

    update: update dashboard component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import '../styles/Dashboard.css'
+import '../styles/Dashboard.css'
 
-function Dashboard(props) {
-  return (
-    <div id='dashboard-page'>
-        <div id='greetings'>
+function Dashboard() {
+    return (
+        <div id='dashboard-page'>
+            {/* <div id='greetings'>
             <p className='heading-highest'>Hi, {props.name}</p>
         </div>
         <div>
@@ -17,9 +17,64 @@ function Dashboard(props) {
             <div className='card' id='resources-card'>
                 <p>Resources</p>
             </div>
-        </div>
-    </div>
-  )
+        </div> */}
+
+            <div class="wrapper3">
+                <div class="static-txt">&nbsp&nbsp&nbspHello, Welcome</div>
+            </div>
+            <div class="wrapper3">
+                <div class="static-txt">&nbsp&nbsp&nbspHere you can</div>
+            </div>
+
+
+
+
+            <div class="wrapper2">
+
+                <div class="static-txt"></div>
+                <ul class="dynamic-txts">
+                    <li><span>Report an Incident</span></li>
+                    <li><span>Get resources</span></li>
+                    <li><span>Talk to the community</span></li>
+
+
+                </ul>
+            </div>
+
+            <br />
+
+            <a rel="noopener" target="_blank" href="https://www.litmus.com/"
+                style="background-color:  #994e8a ; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none; padding: 14px 20px; color: #ffffff; border-radius: 5px; display: inline-block; mso-padding-alt: 0;">
+
+                <span style="mso-text-raise: 15pt;">&nbsp&nbsp&nbsp&nbsp&nbspReport an Incident&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &rarr;</span>
+            </a>
+
+
+            <br />
+            <br />
+            <a rel="noopener" target="_blank" href="https://www.litmus.com/"
+                style="background-color:  #994e8a ; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none; padding: 14px 20px; color: #ffffff; border-radius: 5px; display: inline-block; mso-padding-alt: 0;">
+
+                <span style="mso-text-raise: 15pt;">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspGet
+                    resources&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &rarr;</span>
+
+            </a>
+            <br />
+
+            <br />
+            <a rel="noopener" target="_blank" href="https://www.litmus.com/"
+                style="background-color:  #994e8a ; font-size: 18px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-decoration: none; padding: 14px 20px; color: #ffffff; border-radius: 5px; display: inline-block; mso-padding-alt: 0;">
+
+                <span style="mso-text-raise: 15pt;">Talk to the community &rarr;</span>
+
+            </a>
+
+
+            <br /><br />
+
+        </div >
+    )
 }
 
 export default Dashboard
