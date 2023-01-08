73c29a1561be15d02aa1a6a64861a6769efeffe9
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-01-08 12:06:09 +0530

    update: add routing to different components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Landing from './components/Landing';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register';
+import Resources from './components/Resources';
+import Forum from './components/Forum';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Landing/>
-      <Footer/>
+      <Router>
+        <Routes>
+          <Route path = '/' element={<Landing/>}></Route>
+          <Route path = '/register' element={<Register/>}></Route>
+          <Route path = '/login' element={<Login/>}></Route>
+          <Route path = '/forum' element={<Forum/>}></Route>
+          <Route path = '/resources' element={<Resources/>}></Route>
+        </Routes>
+        </Router>
     </div>
   );
 }
